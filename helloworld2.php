886c7be2f0217443366225cdba289734d4e8a295
Tenzin123-Tech <987475@win.edu.au>
2024-09-09 16:26:55 +1000

    Add files via upload

--- helloworld2.php
+++ helloworld2.php
@@ -1,26 +1,23 @@
 <html>
 <head>
-    <title> Get multiple records from a table example code </title>
+    <title> Get a single record from a table example code </title>
 </head>
 <body>
 <TABLE>
     <TH>Number</TH><TH>Last Name</TH><TH>First Name</TH>
     <?php
-        // $conn = mysqli_connect(“host”,”user”,”password”,”Database”);
+        // $conn = mysqli_connect(“host”,“user”,“password”,“Database”);
         $conn = mysqli_connect("localhost","root","","saleco");
         // Check connection
         if (!$conn) { die("Connection failed: " . mysqli_connect_error()); }
         echo "Connected successfully <br>";
         // once connection established prepare a query
-        $query = "Select * FROM tblcustomer";
+        $query = "Select * FROM tblcustomer where cusCode=10010 ";
         // Run the query
         $result = mysqli_query($conn,$query) or die("Unable to run the query");
         echo("<br> Select returned ". mysqli_num_rows($result)." rows. <br><br>");
-        // Print the result line by line
-        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-            echo "<TR><TD>$row[cusCode]</TD><TD>$row[cusFName]</TD><TD>$row[cusLName]</TD></TR>";
-        }
-        // End of while
+        $row=mysqli_fetch_row($result);
+        echo "<TR><TD>$row[0]</TD><TD>$row[1]</TD><TD>$row[2]</TD></TR>";
         // Close connection to the database
         mysqli_close($conn);
     ?>
